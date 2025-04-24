fb664718efb55f3fa50b16089faae5421df4b2d4
Jaqueline Sales <jaquelinesales200729@gmail.com>
2025-04-24 09:27:55 -0300

    Create for_test.py

---
+++ for_test.py
@@ -0,0 +1,7 @@
+for i in range(20):
+    if (i % 2 == 0):
+        print(f'Números pares: {i}')
+    elif (i % 2 != 0):
+        print(f'Números ímpares: {i}')
+    else:
+        print('valor inválido')
